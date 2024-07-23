ad738e3921d4021282fd7a682f6492ca93103885
kavicii <kavicii.c@gmail.com>
2024-07-23 20:05:06 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var root = document.querySelector(':root');
+var container = document.querySelector('.container');
+var taskList = document.getElementById('tasksList');
+var addTaskBtn = document.getElementById('addTaskBtn');
+var taskCheckBtns = document.querySelectorAll('.task--checked')
+
+//add task
+function addTask(newTask){}
+
+//delete task
+function deleteTask(btns){}
+
+//modify task
+function modifyTask(btns){}
