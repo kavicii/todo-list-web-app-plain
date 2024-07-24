706b7ee1d69295c594128305f03d9c56ae5987db
kavicii <kavicii.c@gmail.com>
2024-07-24 17:03:36 +0800

    feature: add toggle item's edit mode

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var taskCheckBtns = document.querySelectorAll('.task--checked')
 addTaskBtn.addEventListener('click', () => addTask());
 
 //add task
-const addTask = () => {
+function addTask() {
     const newTaskItem = document.createElement('li');
     newTaskItem.setAttribute('class', 'task__item');
     const newTaskBtn = document.createElement('div');
@@ -17,37 +17,61 @@ const addTask = () => {
     newTaskInput.setAttribute('input', 'text');
     const newTaskText = document.createElement('span');
     newTaskText.setAttribute('class', 'task__text');
+    const newTaskMask = document.createElement('div');
+    newTaskMask.setAttribute('class', 'task__mask hidden');
+    const newDelBtn = document.createElement('div');
+    newDelBtn.setAttribute('class', 'task__delete__button button');
+    const newModBtn = document.createElement('div');
+    newModBtn.setAttribute('class', 'task__modify__button button');
 
     taskList.appendChild(newTaskItem);
     newTaskItem.appendChild(newTaskBtn);
     newTaskItem.appendChild(newTaskInput);
     newTaskItem.appendChild(newTaskText);
+    newTaskItem.appendChild(newTaskMask);
+    newTaskMask.appendChild(newDelBtn);
+    newTaskMask.appendChild(newModBtn);
 
     newTaskInput.focus();
-    onClickOutside(newTaskItem, ()=>confirmAddingTask(newTaskInput, newTaskText))
+    onClickOutside(newTaskItem, () => confirmAddingTask(newTaskInput, newTaskText))
+        .then(() => newTaskItem.addEventListener('click', () => toggleTask(newTaskMask)));
 }
 
 //confirm adding task
 const confirmAddingTask = (input, text) => {
-    if(input.value !== ''){
+    if (input.value !== '') {
         text.innerText = input.value;
         input.remove();
-    }else{
+    } else {
         input.parentElement.remove();
     }
 }
 
 //delete task
-const deleteTask = (btns) => {}
+const deleteTask = (btns) => { }
 
 //modify task
-const modifyTask = (btns) => {}
+const modifyTask = (btns) => { }
+
+//toggle task edit mode
+const toggleTask = (el) => {
+    if (el.classList.contains("hidden")) {
+        el.classList.remove("hidden");
+        console.log(el)
+    } else {
+        el.classList.add("hidden");
+        console.log(el)
+    }
+}
 
 //event listener of clicking outside from the element
 const onClickOutside = (el, callback) => {
-    document.addEventListener('click', event => {
-        if (!el.contains(event.target)){
-            callback();
-        } 
-      },{capture: true});
+    return new Promise((resolve) => {
+        document.addEventListener('click', event => {
+            if (!el.contains(event.target)) {
+                callback();
+                resolve();
+            }
+        }, { capture: true });
+    })
 }
